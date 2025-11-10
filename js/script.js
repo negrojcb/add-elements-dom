8c2022736a19f7cdb43b829525d2a2b46b64c187
Juan Cruz <juancbonelli@gmail.com>
2025-11-10 11:26:53 +0100

    modificación script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
- // Aquí tu código
+// Aquí tu código
+const lista = document.getElementById("lista");
+const agregar = document.getElementById("agregar");
+console.log(agregar);
+
+agregar.addEventListener("click", function () {
+  const elemento = prompt("Agregá un elemento a la lista");
+  const item = document.createElement("li");
+  lista.appendChild(item);
+  item.textContent = elemento;
+});
